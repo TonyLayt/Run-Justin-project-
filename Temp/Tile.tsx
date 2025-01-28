3307a0e51ef7ac80bd284904fbf24e7b9310b4e5
TonyLayt <makctony@ukr.net>
2025-01-28 18:33:37 +0200

    add ignore files

--- Temp/Tile.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="vectorMap" tilewidth="32" tileheight="32" tilecount="8393" columns="77">
- <image source="vector/vectorMap.png" width="2480" height="3508"/>
-</tileset>
